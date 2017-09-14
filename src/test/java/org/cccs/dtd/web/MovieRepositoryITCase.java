340cc4293818c93c45ddf9b5fbdd0d7ea2b558df
Craig Cook <craig.cook@code.red>
2017-09-14 10:27:11 +0100

    Got API int test working

--- src/test/java/org/cccs/dtd/web/MovieRepositoryITCase.java
+++ src/test/java/org/cccs/dtd/web/MovieRepositoryITCase.java
@@ -3,11 +3,13 @@ package org.cccs.dtd.web;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import org.cccs.dtd.Application;
 import org.cccs.dtd.domain.Movie;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +19,14 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,21 +38,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MovieRepositoryITCase {
 
     @Autowired
+    private WebApplicationContext context;
     private MockMvc mvc;
+    private MediaType contentType = new MediaType("application", "hal+json", Charset.forName("UTF-8"));
+
 
     @Autowired
     private MovieRepository repository;
 
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
-    @WithMockUser(username = "test", password = "test", roles = "USER")
+    @WithMockUser(username = "user", password = "password", roles = "USER")
     public void whenFindByName_thenReturnMovie_thenStatus200() throws Exception {
         mvc.perform(get("/api/movies/1")
-                .with(testUser())
-                .contentType(MediaType.APPLICATION_JSON))
+                .contentType(contentType))
                 .andExpect(status().isOk())
                 .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].name", is("Star Wars I")));
+                        .contentTypeCompatibleWith(contentType))
+                .andExpect(jsonPath("$.name", is("Star Wars I")));
 
     }
 
